bfaeee5de61f7d61bea9f80e6d9c605e44dd1c61
rizwan933236 <114384405+rizwan933236@users.noreply.github.com>
2023-03-26 23:56:18 +0500

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,61 +1,99 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Register.css'
-// import myimage from '../pictures/homePage.jpeg'
 
 function Register() {
-  let navigate = useNavigate();
-  const [Name, setName] = useState('');
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!formValues.name) {
+      errors.name = 'Name is required';
+    }
+
+    if (!formValues.email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      errors.email = 'Email address is invalid';
+    }
+
+    if (!formValues.password) {
+      errors.password = 'Password is required';
+    } else if (formValues.password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const errors = validateForm();
+
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return;
+    }
+
     try {
-      const result = await fetch("https://dizzy-fly-flannel-nightgown.cyclic.app/register", {
-        
-        method: "POST",
+      const response = await fetch('https://dizzy-fly-flannel-nightgown.cyclic.app/register', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          name: Name,
-          email: Email,
-          password: Password
-        })
+        body: JSON.stringify(formValues)
       });
-      if (result.status === 409) {
-        alert("Email address is already registered");
-      }
-      else{
-        alert("Successfuly Registered");
-        const data = await result.json();
-        console.log(data);
+
+      if (response.status === 409) {
+        alert('Email address is already registered');
+      } else {
+        alert('Successfully registered');
         navigate('/login');
       }
+    } catch (error) {
+      alert('An error occurred. Please try again.');
     }
-    catch (err) {
-      alert("Invalid");
-    }
-  }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }));
+  };
 
   return (
     <div className="register-page">
-       <header>
+      <header>
         <h1>Register</h1>
       </header>
       <form onSubmit={handleSubmit} className="register-form">
         <label>
           Name:
-          <input type="text" value={Name} onChange={(event) => setName(event.target.value)} />
+          <input type="text" name="name" value={formValues.name} onChange={handleChange} />
+          {errors.name && <span className="error">{errors.name}</span>}
         </label>
         <label>
           Email:
-          <input type="email" value={Email} onChange={(event) => setEmail(event.target.value)} />
+          <input type="email" name="email" value={formValues.email} onChange={handleChange} />
+          {errors.email && <span className="error">{errors.email}</span>}
         </label>
         <label>
           Password:
-          <input type="password" value={Password} onChange={(event) => setPassword(event.target.value)} />
+          <input type="password" name="password" value={formValues.password} onChange={handleChange} />
+          {errors.password && <span className="error">{errors.password}</span>}
         </label>
         <button type="submit">Register</button>
       </form>
@@ -64,3 +102,4 @@ function Register() {
 }
 
 export default Register;
+
