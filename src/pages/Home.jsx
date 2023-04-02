e2656038bce5a90375980295d8577ba420940afe
rizwan933236 <114384405+rizwan933236@users.noreply.github.com>
2023-04-03 00:16:31 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bgimage from '../pictures/homePage.jpeg'
+import PieRechartComponent from './Chart'
+
 var sectionStyle = {
   width: "100%",
   height: '750px'
 };
+
 export default function Home() {
+  // const [login, setLogin] = useState(false);
+  const isLoggedIn = () => {
+    const token = localStorage.getItem('Token');
+    return !!token;
+  };
+
   return (
-    <div>
-      <img src={bgimage} alt="Here is " style={sectionStyle} />
-    </div>
-  )
+<>
+
+     {isLoggedIn() ? (
+      
+        <PieRechartComponent />
+      ) : (
+        <img src={bgimage} alt="Here is " style={sectionStyle} />
+      )}
+    </>
+  );
 }
